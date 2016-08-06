cae49f24aa806002401b36f8871874a44838948e
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-08-06 18:39:17 +0200

    [lib] getter, setter and constructor

--- src/osticketPhpClient.php
+++ src/osticketPhpClient.php
@@ -10,21 +10,64 @@ namespace it\thecsea\osticket_php_client;
 
 class osticketPhpClient
 {
+    /**
+     * @var \GuzzleHttp\Client
+     */
     private $client;
     /**
      * @var string
      */
     private $apiKey;
+    /**
+     * @var string
+     */
+    private $url;
 
     /**
      * osticketPhpClient constructor.
      * @param string $apiKey
      */
-    public function __construct($apiKey)
+    public function __construct($apiKey, $url)
     {
         $this->apiKey = $apiKey;
+        $this->url = $url;
         $this->client = new \GuzzleHttp\Client();
     }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @param string $apiKey
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+
+
 
 
 }
